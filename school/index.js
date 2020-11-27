937072d2db64cdf3954cf3f985335461e34faa4a
Siddhart <Siddhart_7@live.nl>
2020-11-27 10:02:43 +0100

    sum things
    sum things

--- school/index.js
+++ school/index.js
@@ -6,6 +6,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 })
 
+function Submit() {
+    CheckDayOfBirth();
+}
+
 
 function UpdateAge() {
     const outputElement = document.getElementById("YMOUTPUT")
@@ -16,22 +20,94 @@ function UpdateAge() {
 
     var curD = new Date()
 
-    if(!((day >= 1 && day<= 31) && (month >= 1 && month <= 12) && (year < curD.getFullYear() && year >= 1900))){
+    if (!((day >= 1 && day <= 31) && (month >= 1 && month <= 12) && (year < curD.getFullYear() && year >= 1900))) {
         outputElement.innerHTML = `<p style="color:red;">Please enter a valid date!</p>`
         return
     }
 
-    var TotalMonths = (((curD.getFullYear() * 12) + (curD.getMonth() +1)) - ((year * 12) + month));
+    var TotalMonths = (((curD.getFullYear() * 12) + (curD.getMonth() + 1)) - ((year * 12) + month));
     var dayDif = (curD.getDate() - day)
 
 
-    if(dayDif > 0){
+    if (dayDif > 0) {
         TotalMonths -= 1
     }
 
     var Years = Math.floor(TotalMonths / 12)
     var Months = Math.floor(TotalMonths % 12)
 
-    outputElement.innerHTML = `<p id="JAAR"><b>${Years}</b> jaar en <b>${Months}</b> maanden.</p>
-    <p id="MAANDEN"><b>${TotalMonths}</b> Maanden</p>`
+    if ((curD.getDate() == day) && ((curD.getMonth() + 1) == month)) {
+        outputElement.innerHTML = `
+        <p class="Celebrate">🎉 Gefeliciteerd! 🎉</p>
+        <p id="JAAR"><b>${Years}</b> jaar en <b>${Months}</b> maanden.</p>
+        <p id="MAANDEN"><b>${TotalMonths}</b> Maanden</p>`
+    } else if ((curD.getMonth() + 1) == month) {
+        outputElement.innerHTML = `
+        <p class="Celebrate">🎉 Gefeliciteerd Maandige! 🎉</p>
+        <p id="JAAR"><b>${Years}</b> jaar en <b>${Months}</b> maanden.</p>
+        <p id="MAANDEN"><b>${TotalMonths}</b> Maanden</p>`
+    } else {
+        outputElement.innerHTML = `
+        <p id="JAAR"><b>${Years}</b> jaar en <b>${Months}</b> maanden.</p>
+        <p id="MAANDEN"><b>${TotalMonths}</b> Maanden</p>`
+    }
+}
+
+function CheckDayOfBirth() {
+    //daclare vars
+    var DayInp = Number(document.getElementById("DAG").value)
+    var MonthInp = Number(document.getElementById("MAAND").value)
+    var YearInp = Number(document.getElementById("JAAR").value)
+    var curD = new Date()
+
+    //check if input is valid
+    if (!((DayInp >= 1 && DayInp <= 31) && (MonthInp >= 1 && MonthInp <= 12) && (YearInp < curD.getFullYear() && YearInp >= 1900))) {
+        return
+    }
+
+    //if (!(DayInp || MonthInp || YearInp)) return;
+
+    //Declare vars
+    var DayString = ""
+    var DateString = `${MonthInp}/${DayInp}/${YearInp}`
+    var date1 = new Date(DateString);
+    var date2 = new Date();
+    var difference = date2.getTime() - date1.getTime();
+
+    //Calculate days between today and input
+    var days = Math.ceil(difference / (1000 * 3600 * 24));
+    var DAYS = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"]
+    var curDay = new Date().getDay() - 1
+    
+    //Modulus of 7 to get rest of days
+    var DayDif = ((days % 7) - 1)
+
+    for (var x = 0; x < DAYS.length; x++) {
+        if (DAYS[x] == DAYS[curDay]) {
+            for (var i = 0; i < x; i++) {
+                DAYS.push(DAYS.shift())
+            }
+        }
+    }
+    if (DayDif < 0) {
+        var a = DayDif * -1
+        DayString = (DAYS[a])
+    } else if (DayDif == 0) {
+        DayString = (DAYS[0])
+    }
+    else {
+        var a = DayDif
+        DayString = (DAYS[DAYS.length - a])
+    }
+
+    console.log(DayString)
+
+    //Check if user is correct
+    var UserInp = document.getElementById('GD').value
+
+    if(UserInp == (DayString.substring(0,2))){
+        document.getElementById('PUSHLOG').innerHTML = `<p style="color:green">Klopt</p>`
+    }else{
+        document.getElementById('PUSHLOG').innerHTML = `<p style="color:red">Klopt Niet</p>`
+    }
 }
--- test.js
+++ test.js
@@ -1,21 +1,48 @@
-var bday = "17 6 1995"
+//CALC LEAP YEAR
+// var now = new Date();
+// var thisYear = 2019
 
-var day = Number(bday.split(" ")[0])
-var month = Number(bday.split(" ")[1])
-var year = Number(bday.split(" ")[2])
+// var isLeap = false;
+// if(!((thisYear % 4) == 0)){
+//     isLeap = false
+// }else if(!((thisYear % 100) == 0)){
+//     isLeap = true
+// }else if(!((thisYear % 400) == 0)){
+//     isLeap = false
+// }else{
+//     isLeap = true
+// }
 
-var curD = new Date()
+// if(isLeap){
+//     console.log("Leap")
+// }else{
+//     console.log("NO Leap")
+// }
 
-var TotalMonths = (((curD.getFullYear() * 12) + (curD.getMonth() +1)) - ((year * 12) + month));
-var dayDif = (curD.getDate() - day)
+//CALC NAME OF DAY OF BIRTHDAY
+var date1 = new Date('11/9/2020');
+var date2 = new Date();
+var difference = date2.getTime() - date1.getTime();
+var days = Math.ceil(difference/(1000 * 3600 * 24));
+var DAYS = ["maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag","zondag"]
+var curDay = new Date().getDay() - 1
+var DayDif = ((days % 7) - 1) // good
 
-
-if(dayDif > 0){
-    TotalMonths -= 1
+for (var x = 0; x < DAYS.length; x++){
+    if(DAYS[x] == DAYS[curDay]){
+        for(var i = 0; i < x; i++){
+            DAYS.push(DAYS.shift())
+        }
+    }
 }
 
-console.log("TotalMonths = " + TotalMonths)
-
-var Years = Math.floor(TotalMonths / 12)
-var Months = Math.floor(TotalMonths % 12)
-console.log("Years: " + Years + " Months : " + Months)
+if(DayDif < 0){
+    var a = DayDif * -1
+    console.log(DAYS[a])
+}else if(DayDif == 0){
+    console.log(DAYS[0])
+}
+else{
+    var a = DayDif
+    console.log(DAYS[DAYS.length - a])
+}

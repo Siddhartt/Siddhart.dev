8797c69080c98fbf63fe55fccc1d86bdd972693e
Siddhart <Siddhart_7@live.nl>
2020-12-02 00:24:04 +0100

    this should work
    this should work

--- Scripts/ForVisual.js
+++ Scripts/ForVisual.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    console.log(document.getElementById('ztc'))
+    console.log(document.getElementById('zti'))
     document.getElementById('ztc').style.cssText  = 'z-index:-10000; position:absolute; left:100000px'
     document.getElementById('zti').style.cssText  = 'z-index:-10000; position:absolute; left:100000px'
 })

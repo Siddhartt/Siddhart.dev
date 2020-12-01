673ab5fcc7a1635b39d3b80eea9f695d5042597e
Siddhart <Siddhart_7@live.nl>
2020-12-02 00:26:57 +0100

    timeout
    timeout

--- Scripts/ForVisual.js
+++ Scripts/ForVisual.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log(document.getElementById('ztc'))
-    console.log(document.getElementById('zti'))
-    document.getElementById('ztc').style.cssText  = 'z-index:-10000; position:absolute; left:100000px'
-    document.getElementById('zti').style.cssText  = 'z-index:-10000; position:absolute; left:100000px'
+    setTimeout(() => {
+        console.log(document.getElementById('ztc'))
+        console.log(document.getElementById('zti'))
+        document.getElementById('ztc').style.cssText  = 'z-index:-10000; position:absolute; left:100000px'
+        document.getElementById('zti').style.cssText  = 'z-index:-10000; position:absolute; left:100000px'
+    }, 3000);
 })

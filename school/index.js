e5038b6597f854536ed44f7b8af2227a1d363266
Siddhart <Siddhart_7@live.nl>
2020-12-04 00:58:06 +0100

    worked on school project
    worked on school project

--- school/index.js
+++ school/index.js
@@ -1,54 +1,63 @@
+//GLOBAL VARS
+var Nums = [];
+
 document.addEventListener("DOMContentLoaded", () => {
+
+    genMathProblem();
+    //add eventlisteners for every age input.
     const YDM_Input = document.getElementsByClassName("AGE")
     for (var x = 0; x < YDM_Input.length; x++) {
         YDM_Input[x].addEventListener('input', UpdateAge);
         YDM_Input[x].addEventListener('propertychange', UpdateAge);
     }
-})
 
-function Submit() {
-    CheckDayOfBirth();
-}
-
-
-function UpdateAge() {
-    const outputElement = document.getElementById("YMOUTPUT")
-
-    var day = Number(document.getElementById("DAG").value)
-    var month = Number(document.getElementById("MAAND").value)
-    var year = Number(document.getElementById("JAAR").value)
-
-    var curD = new Date()
-
-    if (!((day >= 1 && day <= 31) && (month >= 1 && month <= 12) && (year < curD.getFullYear() && year >= 1900))) {
-        outputElement.innerHTML = `<p style="color:red;">Please enter a valid date!</p>`
-        return
+    //make "Doorgaan" button interactible when all inputfields are valid
+    const inputs = document.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('input', CheckValid);
+        inputs[i].addEventListener('propertychange', CheckValid);
     }
 
-    var TotalMonths = (((curD.getFullYear() * 12) + (curD.getMonth() + 1)) - ((year * 12) + month));
-    var dayDif = (curD.getDate() - day)
+    //disable button on DOM loaded
+    document.getElementById('SUBMIT').disabled = true
+})
 
-    console.log(dayDif)
 
-    // if (dayDif > 0) {
-    //     TotalMonths -= 1
-    // }
+//This is the function that will update the age text
+function UpdateAge() {
 
-    if(day < curD.getDate()){
+    //define the output html div
+    const outputElement = document.getElementById("YMOUTPUT");
+
+    //declare main variables
+    var day = Number(document.getElementById("DAG").value);
+    var month = Number(document.getElementById("MAAND").value);
+    var year = Number(document.getElementById("JAAR").value);
+
+    //get today
+    var curD = new Date();
+    var TotalMonths = 0;
+
+    //check if given values are valid. if not return Error
+    if (!((day >= 1 && day <= 31) && (month >= 1 && month <= 12) && (year < curD.getFullYear() && year >= 1900))) {
+        outputElement.innerHTML = `<p style="color:red;">Please enter a valid date!</p>`
+        return;
+    }
+    
+    //Add total months
+    TotalMonths += ((curD.getFullYear() - year) * 12);
+    TotalMonths += ((curD.getMonth() + 1) - month);
+    
+    //correction
+    if(curD.getDate() < day){
         TotalMonths--;
     }
 
-    if(day > curD.getDate()){
-        TotalMonths++;
-    }
+    //calculate years and months for output
+    var Years = Math.floor(TotalMonths / 12);
+    var Months = Math.floor(TotalMonths % 12);
 
-    console.log(TotalMonths)
-
-
-
-    var Years = Math.floor(TotalMonths / 12)
-    var Months = Math.floor(TotalMonths % 12)
-
+    //if statements to check if user is jaarig, maandig or none
     if ((curD.getDate() == day) && ((curD.getMonth() + 1) == month)) {
         outputElement.innerHTML = `
         <p class="Celebrate">🎉 Gefeliciteerd! 🎉</p>
@@ -66,61 +75,126 @@ function UpdateAge() {
     }
 }
 
+//this is the function to calculate the string of the user birthday
 function CheckDayOfBirth() {
-    //daclare vars
-    var DayInp = Number(document.getElementById("DAG").value)
-    var MonthInp = Number(document.getElementById("MAAND").value)
-    var YearInp = Number(document.getElementById("JAAR").value)
-    var curD = new Date()
+
+    //declare vars
+    var DayInp = Number(document.getElementById("DAG").value);
+    var MonthInp = Number(document.getElementById("MAAND").value);
+    var YearInp = Number(document.getElementById("JAAR").value);
+    var curD = new Date();
 
     //check if input is valid
     if (!((DayInp >= 1 && DayInp <= 31) && (MonthInp >= 1 && MonthInp <= 12) && (YearInp < curD.getFullYear() && YearInp >= 1900))) {
-        return
+        return;
     }
 
-    //if (!(DayInp || MonthInp || YearInp)) return;
-
-    //Declare vars
-    var DayString = ""
-    var DateString = `${MonthInp}/${DayInp}/${YearInp}`
+    //vars for date format
+    var DayString = "";
+    var DateString = `${MonthInp}/${DayInp}/${YearInp}`;
     var date1 = new Date(DateString);
     var date2 = new Date();
     var difference = date2.getTime() - date1.getTime();
 
-    //Calculate days between today and input
-    var days = Math.ceil(difference / (1000 * 3600 * 24));
-    var DAYS = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"]
-    var curDay = new Date().getDay() - 1
+    //calculate total days between given date and current date
+    var days = Math.round(difference / (1000 * 3600 * 24));
+    var DAYS = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"];
+    var curDay = new Date().getDay() - 1;
     
-    //Modulus of 7 to get rest of days
-    var DayDif = ((days % 7) - 1)
+    //modulus 7 to get the days that are left
+    var DayDif = ((days % 7));
 
+    //shift array till current daystring is in position 0
     for (var x = 0; x < DAYS.length; x++) {
         if (DAYS[x] == DAYS[curDay]) {
             for (var i = 0; i < x; i++) {
-                DAYS.push(DAYS.shift())
+                DAYS.push(DAYS.shift());
             }
         }
     }
+    //Get the dateString
+
     if (DayDif < 0) {
         var a = DayDif * -1
-        DayString = (DAYS[a])
+        DayString = (DAYS[a]);
     } else if (DayDif == 0) {
-        DayString = (DAYS[0])
+        DayString = (DAYS[0]);
     }
     else {
         var a = DayDif
-        DayString = (DAYS[DAYS.length - a])
+        DayString = (DAYS[DAYS.length - a]);
     }
 
-    console.log(DayString)
+    //check if the user input was correct and return "klopt" if true and "klopt niet" if false
+    var UserInp = document.getElementById('GD').value;
 
-    //Check if user is correct
-    var UserInp = document.getElementById('GD').value
-
+    //return output
     if(UserInp.toLowerCase() == (DayString.substring(0,2).toLowerCase())){
-        document.getElementById('PUSHLOG').innerHTML = `<p style="color:green">Klopt</p>`
+        document.getElementById('PUSHLOG').innerHTML = `<p style="color:green">Klopt</p>`;
     }else{
-        document.getElementById('PUSHLOG').innerHTML = `<p style="color:red">Klopt Niet</p>`
+        document.getElementById('PUSHLOG').innerHTML = `<p style="color:red">Klopt Niet</p>`;
     }
+}
+
+function genMathProblem(){
+
+    for(var x = 0; x < 3; x++){
+        var randNum = Math.floor(Math.random() * 89) + 10;
+        Nums.push(randNum)
+
+        //corrections to make the ui look good
+        switch(x){
+            case(1):
+                randNum += " x";
+                break;
+
+            case(2):
+                randNum += " +";
+                break;
+
+            case(0):
+                document.getElementById(x).style.marginRight = "12px";
+                break;
+        }
+        document.getElementById(x).innerHTML = randNum;
+    }
+}
+
+function CheckMathAnswers(){
+
+    var FirstInp = document.getElementById('KEER')
+    var SecInp = document.getElementById('PLUS')
+    //calculate answers
+    var keer = (Nums[0] * Nums[1])
+    var som = (keer + Nums[2])
+    FirstInp.value = keer
+    SecInp.value = som
+}
+
+function CheckValid(){
+
+    //get all input objects
+    const inputs = document.getElementsByTagName('input');
+    var Enable = true;
+    for (var i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value){
+            Enable = false
+        }
+    }
+
+    //enable or disable button on state
+    if(Enable){
+        document.getElementById('SubmitDiv').className = 'Submit SUB'
+        document.getElementById('SUBMIT').disabled = false
+    }else{
+        document.getElementById('SubmitDiv').className = 'Submit'
+        document.getElementById('SUBMIT').disabled = true
+    }
+}
+
+function Submit() {
+    
+    //Check if all the inputs are valid
+    CheckDayOfBirth();
+    CheckMathAnswers();
 }
